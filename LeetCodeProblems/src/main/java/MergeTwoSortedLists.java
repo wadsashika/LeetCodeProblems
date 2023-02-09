66498ef24f00f0d998501ec00acebbb7057c595f
Dulanga <wadsashika@gmail.com>
2023-02-10 00:07:36 +0800

    Merge Two Sorted Lists Problem Solution - Java

---
+++ LeetCodeProblems/src/main/java/MergeTwoSortedLists.java
@@ -0,0 +1,41 @@
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        MergeTwoSortedLists merge = new MergeTwoSortedLists();
+        ListNode head = merge.mergeTwoLists(list1, list2);
+        System.out.println(head.val);
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+
+        return list1 == null ? list2 : list1;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
